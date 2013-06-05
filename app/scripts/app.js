765561f1f2a052c88e587460060b31c75e0b0a67
Nurefşan <sarikaya.nurefsan@gmail.com>
2013-06-05 16:40:55 +0300

    yeoman init

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('vagrantApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
