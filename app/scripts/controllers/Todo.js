1d9b6e6c0536b273be558610e091b9ef51f0ed47
Nurefşan <sarikaya.nurefsan@gmail.com>
2013-06-06 17:18:20 +0300

    todo style changed

--- app/scripts/controllers/Todo.js
+++ app/scripts/controllers/Todo.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function TodoCtrl($scope) {
-  $scope.todos = [];
+  $scope.todos = [{"text":"asfasf", "index":0},{"text":"asfasasfaf", "index":1},
+  {"text":"asfas1yhhj1hj1j1{][}]|</span>£#$(/f", "index":2}];
 
   $scope.addTodo = function() {
     $scope.todos.push({text:$scope.todoInput, index: $scope.todos.length});
