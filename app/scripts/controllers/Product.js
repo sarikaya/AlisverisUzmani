d9466bab8bc4aa31c9d1cbcc64ffde1540f2b2de
Nurefşan <sarikaya.nurefsan@gmail.com>
2013-06-06 14:54:46 +0300

    Todo and Product Controller Added

---
+++ app/scripts/controllers/Product.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function ProductCtrl($scope) {
+    $scope.productInfo = {
+	"imageSrc": "main.jpg",
+        "name": "ÜLKER ÇİKOLATALI GOFRET 38 GR"
+    };
+    $scope.prices = [
+	{"chainName": "BİM", "branchName": "Bulgurlu", "price": 0.45, "here": true},
+	{"chainName": "A 101", "branchName": "Bulgurlu", "price": 0.45},
+	{"chainName": "Şok", "branchName": "Bulgurlu", "price": 0.57}
+    ];
+}
---
+++ app/scripts/controllers/Todo.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('vagrantApp')
+  .controller('TodoCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
