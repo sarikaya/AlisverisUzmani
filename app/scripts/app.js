32559cd10b714f2bcd09182677e5cafeaeef5d26
namlia <namlia.92@gmail.com>
2013-06-13 01:51:09 +0300

    primitive barcode scanner and develop mocks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,3 +23,25 @@ angular.module('asistanApp', [])
         redirectTo: '/'
       });
   });
+
+angular.module('asistanApp')
+  .run(function ($rootScope, $location) {
+
+    $rootScope.scan = function() {
+
+      function onError(error) {
+        // TODO: when there is a error, redirect user to the search page for 
+        // searching barcode no or name of product
+      }
+
+      window.plugins.barcodeScanner.scan(function(barcode) {
+        if (barcode.cancelled) { 
+          onError();
+        }
+        
+        // TODO: use barcode.text. maybe barcode.format
+        $location.path('/product/' + barcode.text);
+      }, onError);
+      
+    };
+  });
