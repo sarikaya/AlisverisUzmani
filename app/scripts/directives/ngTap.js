c5500ed348979ae6d4d359ba6ef22a6eb8dfff43
namlia <namlia.92@gmail.com>
2013-06-13 20:16:11 +0300

    ngTap bind to click

--- app/scripts/directives/ngTap.js
+++ app/scripts/directives/ngTap.js
@@ -3,7 +3,8 @@
 angular.module('asistanApp')
   .directive('ngTap', function () {
     return function (scope, element, attrs) {
-      $(element).on("tap", function (ev) {
+      // FIXME: bind tap events for ios/safari 300ms delay
+      element.bind("click", function (ev) {
         scope.$apply(attrs['ngTap']);
       });
     };
