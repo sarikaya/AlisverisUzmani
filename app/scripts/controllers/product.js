056ea266c4f92c079236adbcbbf8c2c81be6e09d
namlia <namlia.92@gmail.com>
2013-06-14 13:16:07 +0300

    barcode scanner added to the resolve function of /product/scanitnow route

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -1,8 +1,7 @@
 'use strict';
 
 angular.module('asistanApp')
-  .controller('ProductCtrl', function ($scope, $routeParams, data) {
-    $routeParams.barcode;
+  .controller('ProductCtrl', function ($scope, data) {
     $scope.productInfo = data.productInfo;
     $scope.prices = data.prices;
   });
