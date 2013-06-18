58884fe1f3e005f162096a70c881a2e9d8fd3d94
namlia <namlia.92@gmail.com>
2013-06-19 01:43:09 +0300

    new scan route fix 3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,15 +19,31 @@ angular.module('asistanApp', [])
         templateUrl: 'views/product.html',
         controller: 'ProductCtrl',
         resolve: { // XXX: use inline annotation temporarily until ngmin implement it
-          resp: ['$route', '$http', function ($route, $http) {
+          resp: ['$route', '$http', function ($route, $http, barcodeScanner) {
             var barcodeParam = $route.current.params.barcode;
-            if (barcodeParam !== "scanitnow") {
+            
+            function onScanError(error) {
+              // TODO: when there is a error, redirect user to the search page for 
+              // searching barcode no or name of product
+            }
+            
+            if (barcodeParam === "scanitnow") {              
+              barcodeScanner.scan(function(barcode) {
+              
+                if (barcode.cancelled) {
+                  onScanError();
+                } else {
+                  // TODO: use barcode.text. maybe barcode.format
+                  $location.path('/product/' + barcode.text).replace();
+                  barcodeParam = barcode.text;
+                }
+              }, onScanError);
+            } else {
               // TODO: get geolocation fast (using some geolocation 30 min timeout cache)
               // TODO: get the response promise by using barcodeParam, geolocatin etc...
               // TODO: is resource called in every route change? or only once in the ctrlr
               return $http.post('/product', {"barcode": barcodeParam, "lat": 12312, "long": 234234});
             }
-            return {};
           }]
         }
       })
--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -1,27 +1,7 @@
 'use strict';
 
 angular.module('asistanApp')
-  .controller('ProductCtrl', function ($scope, $routeParams, $location, barcodeScanner, resp) {
-  
-    if ($routeParams.barcode === "scanitnow") {
-    
-      function onScanError(error) {
-        // TODO: when there is a error, redirect user to the search page for 
-        // searching barcode no or name of product
-      }
-      
-      barcodeScanner.scan(function(barcode) {
-      
-        if (barcode.cancelled) {
-          onScanError();
-        } else {
-          // TODO: use barcode.text. maybe barcode.format
-          $location.path('/product/' + barcode.text).replace();
-        }
-      }, onScanError);
-    
-    } else {
-    
+  .controller('ProductCtrl', function ($scope, resp) {
       // the result of $http  (`resp` parameter)
       // contains .data, .status, .headers, .config
       $scope.productInfo = resp.data.productInfo;
