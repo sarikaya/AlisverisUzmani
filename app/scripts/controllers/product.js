bc9ce7f3e6811d5eed9ac8285649146586a68cd6
namlia <namlia.92@gmail.com>
2013-06-19 01:50:51 +0300

    new scan route fix 6

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -2,8 +2,10 @@
 
 angular.module('asistanApp')
   .controller('ProductCtrl', function ($scope, resp) {
-      // the result of $http  (`resp` parameter)
-      // contains .data, .status, .headers, .config
-      $scope.productInfo = resp.data.productInfo;
-      $scope.prices = resp.data.prices;
+      if (resp) {
+        // the result of $http  (`resp` parameter)
+        // contains .data, .status, .headers, .config
+        $scope.productInfo = resp.data.productInfo;
+        $scope.prices = resp.data.prices;
+      }
   });
