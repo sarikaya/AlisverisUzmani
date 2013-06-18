49beed870164185013ff4d689baacc1963e1ffb4
namlia <namlia.92@gmail.com>
2013-06-19 00:58:53 +0300

    when tapping to the scan again in the scan cannot open scaner fixed

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -1,9 +1,30 @@
 'use strict';
 
 angular.module('asistanApp')
-  .controller('ProductCtrl', function ($scope, resp) {
-    // the result of $http  (`resp` parameter)
-    //  contains .data, .status, .headers, .config
-    $scope.productInfo = resp.data.productInfo;
-    $scope.prices = resp.data.prices;
+  .controller('ProductCtrl', function ($scope, $routeParams, $location, resp) {
+  
+    if ($routeParams.barcode === "scanitnow") {
+    
+      function onScanError(error) {
+        // TODO: when there is a error, redirect user to the search page for 
+        // searching barcode no or name of product
+      }
+      
+      barcodeScanner.scan(function(barcode) {
+      
+        if (barcode.cancelled) {
+          onScanError();
+        } else {
+          // TODO: use barcode.text. maybe barcode.format
+          $location.path('/product/' + barcode.text).replace();
+        }
+      }, onScanError);
+    
+    } else {
+    
+      // the result of $http  (`resp` parameter)
+      // contains .data, .status, .headers, .config
+      $scope.productInfo = resp.data.productInfo;
+      $scope.prices = resp.data.prices;
+    }
   });
