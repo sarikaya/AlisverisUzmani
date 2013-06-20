59a2511638fa55e76f1b4050306e3a0aeddf72a6
Nurefşan <sarikaya.nurefsan@gmail.com>
2013-06-20 22:04:56 +0300

    MongoDb added

--- boot.sh
+++ boot.sh
@@ -63,6 +63,20 @@ else
     
     # install dependencies in the package.json --globaly
     sudo npm install
+
+
+    #########  mongodb  ############
+
+    apt-packages-repository 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' '7F0CEB10'
+    apt-packages-update
+    apt-packages-install mongodb-10gen
+
+    if [ ! -d '/data' ]; then
+      sudo mkdir /data
+      sudo mkdir /data/db
+      sudo mkdir /data/db/journal
+      sudo chown -R vagrant:vagrant /data
+    fi
     
 
 
