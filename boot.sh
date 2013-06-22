d370e189ecccbd937a7c702f20473082e5b250a3
namlia <namlia.92@gmail.com>
2013-06-22 22:53:28 +0300

    pymongo installation added to boot.sh

--- boot.sh
+++ boot.sh
@@ -68,7 +68,8 @@ else
 
     apt-packages-repository 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' '7F0CEB10'
     apt-packages-update
-    apt-packages-install mongodb-10gen
+    apt-packages-install mongodb-10gen python-pip
+    pip install pymongo
 
     if [ ! -d '/data' ]; then
       sudo mkdir /data
