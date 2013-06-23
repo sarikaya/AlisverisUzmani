dbfd053a100334790fc794d9767ad16334dc5bc1
namlia <namlia.92@gmail.com>
2013-06-23 21:50:55 +0300

    scanner controller added for barcodeScanner

--- app/scripts/controllers/scanner.js
+++ app/scripts/controllers/scanner.js
@@ -1,10 +1,18 @@
 'use strict';
 
 angular.module('asistanApp')
-  .controller('ScannerCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ScannerCtrl', function ($scope, $location, barcodeScanner) {
+    function onScanError(error) {
+      // TODO: when there is a error, redirect user to the search page for 
+      // searching barcode no or name of product
+      return {}
+    }  
+    barcodeScanner.scan(function(barcode) {
+      if (barcode.cancelled) {
+          onScanError();
+      } else {
+        // TODO: use barcode.text. maybe barcode.format
+        $location.path('/product/' + barcode.text);
+      }
+    }, onScanError);
   });
