c1d4adf3f1a06642aa404df0d897e826e0f042b9
namlia <namlia.92@gmail.com>
2013-06-24 01:59:06 +0300

    scanner view and controller cleaned

--- app/scripts/controllers/scanner.js
+++ app/scripts/controllers/scanner.js
@@ -11,7 +11,7 @@ angular.module('asistanApp')
       if (barcode.cancelled) {
           onScanError();
       } else {
-        // TODO: use barcode.text. maybe barcode.format
+        // TODO: maybe you need barcode.format for different barcode types
         $location.path('/product/' + barcode.text);
       }
     }, onScanError);
